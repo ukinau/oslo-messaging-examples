b96382118debebcc14301f593398c0f4ce71ca53
Hiroyasu.OHYAMA <user.localhost2000@gmail.com>
2016-08-28 17:25:45 +0900

    implement client processing to call RPC which is defined in server.py

---
+++ src/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from oslo_config import cfg
+import oslo_messaging 
+
+URL = 'rabbit://guest:guest@localhost:5672/'
+
+class TestClient(object):
+  def __init__(self, transport):
+    target = oslo_messaging.Target(topic='test01')
+    self.client = oslo_messaging.RPCClient(transport, target)
+
+  def hoge(self, ctxt, arg):
+    return self.client.call(ctxt, 'hoge', arg=arg)
+
+  def fuga(self, ctxt, arg):
+    cctxt= self.client.prepare(namespace='foo', version='1.1')
+    return cctxt.call(ctxt, 'fuga', arg = arg)
+
+transport = oslo_messaging.get_transport(cfg.CONF, url = URL)
+client = TestClient(transport)
+
+print(client.hoge({}, 10))
+print(client.fuga({}, 10))
