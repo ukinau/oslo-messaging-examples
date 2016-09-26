328aa2eb820f402a28552a2ad4bfbffea77ac096
Hiroyasu.OHYAMA <user.localhost2000@gmail.com>
2016-09-26 11:19:55 +0900

    changed notifier_server to make it easy to test

--- src/notifier_server.py
+++ src/notifier_server.py
@@ -3,44 +3,43 @@
 from oslo_config import cfg
 import oslo_messaging 
 
-URL = 'rabbit://guest:guest@localhost:5672/'
-
 class HogeEndpoint(object):
-    filter_rule = oslo_messaging.NotificationFilter(event_type='.*: hoge$')
+  filter_rule = oslo_messaging.NotificationFilter(event_type='.*: hoge$')
 
-    def info(self, ctxt, publisher_id, event_type, payload, metadata):
-        print("[HogeEndpoint] ctxt: %s" % (ctxt))
-        print("[HogeEndpoint] publisher_id: %s" % (publisher_id))
-        print("[HogeEndpoint] event_type: %s" % (event_type))
-        print("[HogeEndpoint] payload: %s" % (payload))
-        print("[HogeEndpoint] metadata: %s" % (metadata))
+  def info(self, ctxt, publisher_id, event_type, payload, metadata):
+    print("[HogeEndpoint] ctxt: %s" % (ctxt))
+    print("[HogeEndpoint] publisher_id: %s" % (publisher_id))
+    print("[HogeEndpoint] event_type: %s" % (event_type))
+    print("[HogeEndpoint] payload: %s" % (payload))
+    print("[HogeEndpoint] metadata: %s" % (metadata))
 
 
 class FugaEndpoint(object):
+  def info(self, ctxt, publisher_id, event_type, payload, metadata):
+    print("[FugaEndpoint] ctxt: %s" % (ctxt))
+    print("[FugaEndpoint] publisher_id: %s" % (publisher_id))
+    print("[FugaEndpoint] event_type: %s" % (event_type))
+    print("[FugaEndpoint] payload: %s" % (payload))
+    print("[FugaEndpoint] metadata: %s" % (metadata))
 
-    def info(self, ctxt, publisher_id, event_type, payload, metadata):
-        print("[FugaEndpoint] ctxt: %s" % (ctxt))
-        print("[FugaEndpoint] publisher_id: %s" % (publisher_id))
-        print("[FugaEndpoint] event_type: %s" % (event_type))
-        print("[FugaEndpoint] payload: %s" % (payload))
-        print("[FugaEndpoint] metadata: %s" % (metadata))
 
+def start_server(endpoints=[], topic='test_topic', url=''):
+  transport = oslo_messaging.get_notification_transport(cfg.CONF, url=url)
+  targets   = [oslo_messaging.Target(topic=topic)]
+  server    = oslo_messaging.get_notification_listener(transport, targets, endpoints)
 
-transport = oslo_messaging.get_notification_transport(cfg.CONF, url=URL)
-targets   = [oslo_messaging.Target(topic='test_topic')]
-endpoints = [
-    HogeEndpoint(),
-    FugaEndpoint(),
-]
-
-server = oslo_messaging.get_notification_listener(transport, targets, endpoints)
-
-try:
+  try:
     server.start()
     server.wait()
-except KeyboardInterrupt:
+  except KeyboardInterrupt:
     print("stop server")
     server.stop()
-except Exception as e:
+  except Exception as e:
     print("[ERROR] faield to start server '%s'" % (e.message))
     server.stop()
+
+if __name__ == '__main__':
+  # parse CLI parameter and load configuration file
+  cfg.CONF()
+
+  start_server([HogeEndpoint(), FugaEndpoint()])
